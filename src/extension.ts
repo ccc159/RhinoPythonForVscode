65ac5029ca4fcc88e4bfb709de2c4ea7776a7763
Jingcheng Chen <chen@designtoproduction.com>
2018-05-16 16:29:20 +0200

    changed string message to json message to carry more params

--- src/extension.ts
+++ src/extension.ts
@@ -8,17 +8,48 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    
+    // load user settings
     const RhinoPythonConfig = vscode.workspace.getConfiguration('RhinoPython');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.CodeSender', () => {
+
+    // send the messgage to Rhino
+    var net = require('net');
+    var client = new net.Socket();
+    function SendToRhino (messgage: string) {
+        function onConnectionDisplay () {
+            vscode.debug.activeDebugConsole.append('\n');
+            vscode.debug.activeDebugConsole.appendLine(`@ ====== ${(new Date()).toLocaleString()} ======`);
+            vscode.debug.activeDebugConsole.append('\n');
+        }
+        client.connect(614, '127.0.0.1', function() {
+            if (RhinoPythonConfig.PreserveLog) {
+                onConnectionDisplay();
+            } else {
+                vscode.commands.executeCommand('workbench.debug.panel.action.clearReplAction').then(() => onConnectionDisplay());
+            }
+            client.write(messgage);
+        });
+    }
+
+    client.on('data', function(data: Buffer) {
+        vscode.debug.activeDebugConsole.append(data.toString());
+        vscode.debug.activeDebugConsole.append('\n');
+    });
+
+    // Add a 'close' event handler for the client socket
+    client.on('close', function() {
+        // console.log('Rhino disconnected.');
+    });
+
+    client.on('error', function(err: object) {
+        vscode.debug.activeDebugConsole.appendLine(err.toString());
+        vscode.debug.activeDebugConsole.appendLine('Please make sure Rhino is running CodeListener.');
+    });
+
+    // register execute command
+    let disposable = vscode.commands.registerCommand('extension.CodeSender', args => {
         // check if rhino python is enabled by the user
         if (!RhinoPythonConfig.Enabled) { return; }
-        
+
         // check if editor is open
         let editor = vscode.window.activeTextEditor;
         if (!editor) {
@@ -35,57 +66,50 @@ export function activate(context: vscode.ExtensionContext) {
             // initialize filesystem, operation system, and socket
             var fs = require('fs');
             var os = require('os');
-            var net = require('net');
-            var client = new net.Socket();
+
+            // check if reset engine
+            let reset = args.reset ? true : false;
+
             // check if it is temp file, if yes then save to a temp file
             let temp = editor.document.isUntitled;
+            let run = true;
             if (temp) {
                 var tmpfolder = os.tmpdir();
-                var filename = tmpfolder + "\\TempScript.py";
+                let filename = tmpfolder + "\\TempScript.py";
                 fs.writeFileSync(filename, text);
-                SendToRhino(filename);
+                let msgObject = JSON.stringify({ reset, temp, filename, run });
+                SendToRhino(msgObject);
             } else {
-                let fileName = editor.document.fileName;
-                editor.document.save().then(() => SendToRhino(fileName));
+                let filename = editor.document.fileName;
+                let msgObject = JSON.stringify({ reset, temp, filename, run });
+                editor.document.save().then(() => SendToRhino(msgObject));
             }
         }
-
-        // send the python script file path to Rhino
-        function SendToRhino (path: string) {
-            function onConnectionDisplay () {
-                vscode.debug.activeDebugConsole.append('\n');
-                vscode.debug.activeDebugConsole.appendLine(`@ ====== ${(new Date()).toLocaleString()} ======`);
-                vscode.debug.activeDebugConsole.append('\n');
-            }
-            client.connect(614, '127.0.0.1', function() {
-                if (RhinoPythonConfig.PreserveLog) {
-                    onConnectionDisplay();
-                } else {
-                    vscode.commands.executeCommand('workbench.debug.panel.action.clearReplAction').then(() => onConnectionDisplay());
-                }
-                client.write(path);
-            });
-        }
-
-        client.on('data', function(data: Buffer) {
-            vscode.debug.activeDebugConsole.append(data.toString());
-            vscode.debug.activeDebugConsole.append('\n');
-        });
-        
-        // Add a 'close' event handler for the client socket
-        client.on('close', function() {
-            // console.log('Rhino disconnected.');
-        });
-        
-        client.on('error', function(err: object) {
-            vscode.debug.activeDebugConsole.appendLine(err.toString());
-            vscode.debug.activeDebugConsole.appendLine('Please make sure Rhino is running CodeListener.');
-        });
-
     });
 
     context.subscriptions.push(disposable);
+
+    // register reset and execute command
+    disposable = vscode.commands.registerCommand('extension.CodeSenderResetNRun', () => {
+        vscode.commands.executeCommand('extension.CodeSender', {reset : true});
+    });
+
+    context.subscriptions.push(disposable);
+
+    // register reset command
+    disposable = vscode.commands.registerCommand('extension.CodeSenderReset', () => {
+        let run = false;
+        let filename = "";
+        let temp = true;
+        let reset = true;
+        let msgObject = JSON.stringify({ reset, temp, filename, run });
+        SendToRhino(msgObject);
+    });
+
+    context.subscriptions.push(disposable);
+
 }
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {
